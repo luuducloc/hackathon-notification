0a4af33038ec13e8eba257283786d748b9d8ec51
locluu <luuducloc1997@gmail.com>
2023-03-03 14:14:23 +0700

    Init code base

---
+++ config/server.js
@@ -0,0 +1,42 @@
+import convict from 'convict'
+const serverConfig = convict({
+    env: {
+        doc: 'The application environment.',
+        format: ['production', 'development', 'test'],
+        default: 'development',
+        env: 'NODE_ENV',
+    },
+    mongodb: {
+        url: {
+            doc: 'URL for mongodb connection',
+            format: String,
+            nullable: false,
+            default: 'mongodb://localhost:27017/?directConnection=true',
+            env: 'MONGO_DB_URL',
+        },
+    },
+    redis: {
+        redisPort: {
+            doc: 'Redis port',
+            format: Number,
+            default: 6379,
+            env: 'REDIS_PORT',
+        },
+        redisHost: {
+            doc: 'Redis host',
+            format: String,
+            default: '127.0.0.1',
+            env: 'REDIS_HOST',
+        },
+        redisPassword: {
+            doc: 'Redis password',
+            format: String,
+            default: '',
+            env: 'REDIS_PASSWORD',
+        },
+    },
+})
+
+serverConfig.validate({ allowed: 'strict' })
+
+export default serverConfig
---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from 'cors'
+import 'express-async-errors'
+import restRouter from 'express-rest-routerss'
+import 'dotenv/config'
+import * as rTracer from 'cls-rtracer'
+import { logger, middlewareLogger } from '@root/config/logger'
+import { HTTP_STATUS } from '@root/utils/constants'
+
+const app = express()
+
+// Enable CORS and body parse
+app.use(cors(), express.json(), express.urlencoded({ extended: true }), rTracer.expressMiddleware())
+
+process.on('uncaughtException', (error) => {
+  logger.error(error)
+})
+
+app.use(middlewareLogger)
+
+app.use(restRouter({ routeDir: '/routes' }))
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  logger.error(error)
+  return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+    message: 'Internal server error',
+  })
+})
+
+const port = process.env.PORT || 3000
+const host = process.env.HOST || 'localhost'
+
+app.listen(port, () => {
+  logger.info(`Listening: http://${host}:${port}`)
+})
